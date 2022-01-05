74a697698b588bc09be3bde92a69d5ad1bfe9844
trastanechora <trastanechora@gmail.com>
2022-01-05 14:12:36 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Demo Lighthouse Audit on User Flow
+
+Please install the dependencies and Chromium then run
+
+```
+node select-a-product-after.js
+```
---
+++ select-a-product-after.js
@@ -0,0 +1,290 @@
+const puppeteer = require("puppeteer");
+const { startFlow } = require("lighthouse/lighthouse-core/fraggle-rock/api.js");
+const fs = require("fs");
+
+(async () => {
+  const width = 1350;
+  const height = 940;
+
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: null,
+  });
+  const page = await browser.newPage();
+
+  const flow = await startFlow(page, {
+    name: "Go to homepage",
+    configContext: {
+      settingsOverrides: {
+        screenEmulation: {
+          mobile: false,
+          width,
+          height,
+          deviceScaleFactor: 1,
+          disabled: false,
+        },
+        formFactor: "desktop",
+      },
+    },
+  });
+
+  async function waitForSelectors(selectors, frame) {
+    for (const selector of selectors) {
+      try {
+        return await waitForSelector(selector, frame);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    throw new Error(
+      "Could not find element for selectors: " + JSON.stringify(selectors)
+    );
+  }
+
+  async function waitForSelector(selector, frame) {
+    if (selector instanceof Array) {
+      let element = null;
+      for (const part of selector) {
+        if (!element) {
+          element = await frame.waitForSelector(part);
+        } else {
+          element = await element.$(part);
+        }
+        if (!element) {
+          throw new Error("Could not find element: " + part);
+        }
+        element = (
+          await element.evaluateHandle((el) =>
+            el.shadowRoot ? el.shadowRoot : el
+          )
+        ).asElement();
+      }
+      if (!element) {
+        throw new Error("Could not find element: " + selector.join("|"));
+      }
+      return element;
+    }
+    const element = await frame.waitForSelector(selector);
+    if (!element) {
+      throw new Error("Could not find element: " + selector);
+    }
+    return element;
+  }
+
+  async function waitForElement(step, frame) {
+    const count = step.count || 1;
+    const operator = step.operator || ">=";
+    const comp = {
+      "==": (a, b) => a === b,
+      ">=": (a, b) => a >= b,
+      "<=": (a, b) => a <= b,
+    };
+    const compFn = comp[operator];
+    await waitForFunction(async () => {
+      const elements = await querySelectorsAll(step.selectors, frame);
+      return compFn(elements.length, count);
+    });
+  }
+
+  async function querySelectorsAll(selectors, frame) {
+    for (const selector of selectors) {
+      const result = await querySelectorAll(selector, frame);
+      if (result.length) {
+        return result;
+      }
+    }
+    return [];
+  }
+
+  async function querySelectorAll(selector, frame) {
+    if (selector instanceof Array) {
+      let elements = [];
+      let i = 0;
+      for (const part of selector) {
+        if (i === 0) {
+          elements = await frame.$$(part);
+        } else {
+          const tmpElements = elements;
+          elements = [];
+          for (const el of tmpElements) {
+            elements.push(...(await el.$$(part)));
+          }
+        }
+        if (elements.length === 0) {
+          return [];
+        }
+        const tmpElements = [];
+        for (const el of elements) {
+          const newEl = (
+            await el.evaluateHandle((el) =>
+              el.shadowRoot ? el.shadowRoot : el
+            )
+          ).asElement();
+          if (newEl) {
+            tmpElements.push(newEl);
+          }
+        }
+        elements = tmpElements;
+        i++;
+      }
+      return elements;
+    }
+    const element = await frame.$$(selector);
+    if (!element) {
+      throw new Error("Could not find element: " + selector);
+    }
+    return element;
+  }
+
+  async function waitForFunction(fn) {
+    let isActive = true;
+    setTimeout(() => {
+      isActive = false;
+    }, 5000);
+    while (isActive) {
+      const result = await fn();
+      if (result) {
+        return;
+      }
+      await new Promise((resolve) => setTimeout(resolve, 100));
+    }
+    throw new Error("Timed out");
+  }
+  {
+    const targetPage = page;
+    await targetPage.setViewport({ width, height });
+    // await flow.navigate("https://www.tokopedia.com/");
+  }
+  {
+    const targetPage = page;
+    const promises = [];
+    promises.push(targetPage.waitForNavigation());
+    await targetPage.goto("https://www.tokopedia.com/");
+    await Promise.all(promises);
+    await flow.snapshot({ stepName: "Home page rendered" });
+  }
+  {
+    const targetPage = page;
+    const element = await waitForSelectors(
+      [
+        ["aria/Bidang pencarian"],
+        ["#search-container > form > div > div > div > input"],
+      ],
+      targetPage
+    );
+    await element.click({ offset: { x: 69, y: 16 } });
+
+    await flow.snapshot({ stepName: "Search box input clicked" });
+  }
+  {
+    const targetPage = page;
+    const element = await waitForSelectors(
+      [
+        ["aria/Bidang pencarian"],
+        ["#search-container > form > div > div > div > input"],
+      ],
+      targetPage
+    );
+    const type = await element.evaluate((el) => el.type);
+    if (
+      [
+        "textarea",
+        "select-one",
+        "text",
+        "url",
+        "tel",
+        "search",
+        "password",
+        "number",
+        "email",
+      ].includes(type)
+    ) {
+      await element.type("macbook pro");
+    } else {
+      await element.focus();
+      await element.evaluate((el, value) => {
+        el.value = value;
+        el.dispatchEvent(new Event("input", { bubbles: true }));
+        el.dispatchEvent(new Event("change", { bubbles: true }));
+      }, "macbook pro");
+    }
+    await flow.snapshot({ stepName: "Type into search box input" });
+  }
+  {
+    const targetPage = page;
+    const promises = [];
+    promises.push(targetPage.waitForNavigation());
+    const element = await waitForSelectors(
+      [
+        [
+          "#search-container > div > div > div > a:nth-child(1) > div > div > span > strong:nth-child(1)",
+        ],
+      ],
+      targetPage
+    );
+    await element.click({ offset: { x: 31, y: 7.96875 } });
+    await Promise.all(promises);
+    await flow.snapshot({ stepName: "After Click first row result" });
+  }
+  {
+    const targetPage = page;
+    await targetPage.evaluate(
+      (x, y) => {
+        window.scroll(x, y);
+      },
+      0,
+      290
+    );
+    await flow.snapshot({ stepName: "Do scroll" });
+  }
+  {
+    await flow.startTimespan({ stepName: "Rendering" });
+    const targetPage = page;
+    const element = await waitForSelectors(
+      [
+        ["aria/Lihat Produk"],
+        [
+          "#catalog-catalogInfo > div.css-svbnq2 > div > div.css-q452l1 > button",
+        ],
+      ],
+      targetPage
+    );
+    await element.click({ offset: { x: 68.90625, y: 35 } });
+    await flow.endTimespan();
+    await flow.snapshot({ stepName: "Click show products" });
+  }
+  {
+    const targetPage = page;
+    await targetPage.evaluate(
+      (x, y) => {
+        window.scroll(x, y);
+      },
+      0,
+      2646
+    );
+    await flow.snapshot({ stepName: "Auto scroll to products" });
+  }
+  {
+    const targetPage = page;
+    const promises = [];
+    promises.push(targetPage.waitForNavigation());
+    const element = await waitForSelectors(
+      [
+        [
+          'aria/Apple Macbook Pro 2020 M1 8GB 512GB SSD MYD92 MYDC2 Gray Silver Resmi - c8df[role="img"]',
+        ],
+        [
+          "#\\31 342266025 > div > div > div > div > div.css-zimbi > a > div > img",
+        ],
+      ],
+      targetPage
+    );
+    await element.click({ offset: { x: 46.5625, y: 71.28125 } });
+    await Promise.all(promises);
+    await flow.snapshot({ stepName: "Select a product" });
+  }
+
+  const report = flow.generateReport();
+  fs.writeFileSync("report.html", report);
+  await browser.close();
+})();
